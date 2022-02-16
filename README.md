f98234f0c6dff0e801af9abdcb6a7273e7ec6711
eduschelle <eduschelle@hotmail.com>
2022-02-16 14:28:49 -0300

    commit 2

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
-# Bible_Datamining
-My first readme
+---
+output:
+  html_document:
+    theme: yeti
+    toc: yes
+    toc_float: yes
+  pdf_document: default
+  word_document:
+    toc: yes
+---
+## Bible Datamining
+***
+This analysis uses the tidy approach elaborated in 'Text Mining with R' exploring the King James Version of the Bible.
+
+### Old and new testament analysis
+***
+The Bible, a Jewish-Christian collection of books, consistis of the Old and New Testaments.
+
+There is a very common sense about the strucuture of this testaments, for the old testament being a dark narative, fullfilled by war, sadness and sorrows. Otherwise, the new testament is joyfull most of the time.
+
+The sentiment analysis tools of the data science can give us a taste, if not even prove hypotheses like the above.
+
+In this approach, it was used the lexicans of English words rated for valence with an integer between minus five (negative) and plus five (positive). The words have been manually labeled by Finn Årup Nielsen in 2009-2011. Crossing the words from each book of the Bible with the "Afinn" lexican, there is the possibility to measure some kind of positivity of the texts e rank them by the sum of the score by book. In this analysis, the score was divided by the number of words of the respective book, so the lenght of the book shall not be a variable, and multiplied by 1000, to make the notation easier.
+
+![](C:\Users\Eduardo\projects\bible\project_text_mining\Bible_Datamining\sumarize_of_all_books.png "Fig 1")
+
+
+By the colors spreading of this bar plot, it is intuitive to say that the new testament has books with highter scores than the old. In other words, there is more pleasant texts in the new testament, according to the afinn studies.
+
+Of course, there is no possibility of just mine the text this type of scripture without taking account of the context. The original languages of this books are distant from our modern speech. They have many diferences in structure, number of words, oral tradition, etc...
+
+### Romans analysis
+***
+In a conversation with a friend of mine some time ago, I listened this sentense: "The Romans epistle have a dramatic curve in its narative!". Dramatic curve is the "level of tension" during any exposion (movies, audios, books).
+This argument make me curious about testing it with data mining.
+
+Here some exemplo of a hipothetic dramatic curve:
+
+![](https://magisterwernegren.files.wordpress.com/2017/11/dramatic-curve.jpg "Fig 2")
+
+In this analysis, I used the same lexican of the old and new testament analysis (afinn), supposing that the negativity of the words have strong correlation with the tension of the reader.
+
+Without surprising my friend, I plot a graphic that certainly show a curve:
+
+![](C:\Users\Eduardo\projects\bible\project_text_mining\Bible_Datamining\Romans_plot.png "Fig 3")
+
+There's a climax in the chapters 6 and 7. Reading the text, we can find some words like adulteress, law, dead, death and so on. It is the really tense part of the epistle. If the lexican dispose any of this words, it may add some negative scores to this chapters.
+
+
+## Final considerations
+***
+The sentimental analysis is attached with the object of the study. It can be a book, music, or tweets from any database, it will always demand some level of expertise from the data professional to extract the right information.
+
+This simple analysis shows the data science applied in explore in the dramaturgy field.
+
+
+
+
+
+
+
+
+
 
--- schelle_analysis_bible.R
+++ schelle_analysis_bible.R
@@ -2,82 +2,80 @@ library(tidyverse)
 library(stringr)
 library(readr)
 
-#mudando para diret?rio com o arquivo que cont?m o arquivo de texto
-setwd("C:/Users/Eduardo/projects/bible/project_text_mining")
-getwd() #checar o diret?rio certo
-list.files() #checar se o arquivo est? nesse diret?rio
+#seting the directory to find the text
+setwd("C:/Users/Eduardo/projects/bible/project_text_mining/Bible_Datamining")
+getwd() #check the right directory
+list.files() #check list
 
 
-#Lendo o arquivo com a b?blia em bloco de notas
+#Reading the text note
 filename <- "kjvdat.txt"
 fullpath <- file.path(getwd(), filename)
-file.exists(filename) #existe um arquivo chamado "kjvdat.txt" no path "~/../projects/..."
+file.exists(filename) #there's a file named "kjvdat.txt" in the path "~/../projects/..."
 
-#teste
-read_lines("kjvdat.txt", n_max = 3) #prmeiras linhas
+#test
+read_lines("kjvdat.txt", n_max = 3)
 
-#criando tidy data
+#creating tidy data
 bible_delim <- read_delim(filename, "|") %>%
-  setNames(c("Book", "Chapter", "Verse", "Text")) #delimita??o de coluna pelo "|" e nomes
+  setNames(c("Book", "Chapter", "Verse", "Text")) #separating the columns by the "|" sign
 
 
-bible_delim[which.max(nchar(bible_delim$Text)),1:4] %>% print() #maior versículo em número de caracteres na versão kj 1611
+bible_delim[which.max(nchar(bible_delim$Text)),1:4] %>% print() %>% 
+  pull() #the biggest verse in the kj 1611 version
 
-#Achando linhas em que acaba o velho testamento e come?a o novo
-bible_delim[which(bible_delim$Book == "Mat")] #erro = na posi??o 1 tem index = 23146
-bible_delim[23146,1:4] # primeiro cap de mateus
-bible_delim[23145,1:4] # ?ltimo vers?culo de malaquias
-nrow(bible_delim) #N?mero de vers?culos da b?blia
+#Finding the limit between old and new testement
+bible_delim[which(bible_delim$Book == "Mat")] #error in the position index = 23146
+bible_delim[23146,1:4] # First Mathew index
+bible_delim[23145,1:4] # last Malachi index
+nrow(bible_delim) #Length of indexes = number of verses in the bible
 
-#criando coluna com os testamentos
+#Creating a vector of textament
 
 Testament <- c(rep("Old", 23145), rep("New", length = 31102-23145)) %>%
   setNames(c("Testament"))
-length(Testament) # 31102 = n?mero de vers?culos da b?blia
+length(Testament) # 31102 = number of verses in the bible
 
-#agrupando as colunas da b?blia com o testamento respectivo de cada livro e transformando em fator
+#Biding the columns of the testament and the bible
 bible_w_testament <- bind_cols(bible_delim, as_tibble(Testament)) %>%
   as.data.frame() %>% mutate(Testament = factor(value)) %>% select(1:4,6)
 
-#adicionando uma coluna com a soma do n?mero de palavras por livro
+#adding the collumn with the sum of the words per book
 bible_w_testament <- bible_w_testament %>% group_by(Book) %>% mutate(n_words_by_books = sum(str_count(Text) - 1))
-# "-1" ? porque o texto tinha um "~" no fim de cada linha
+# "-1" Because there is a "~" in the end of each verse
 
-### An?lise de sentimentos
+### Sentiment Analysis
 library(SentimentAnalysis)
 library(tidytext)
 
-nrc<-get_sentiments("nrc") # l?xico com 10 tipos de sentimento
-afinn<-get_sentiments("afinn") # l?xico que pontua entre -5 e +5 cada palavra
+nrc<-get_sentiments("nrc") # 10 groups os sentiment lexican
+afinn<-get_sentiments("afinn") # scores from -5 to +5 lexican
 
-#todas as palavras da b?blia sem contar stop_words
-#precisa desagrupar por livro porque se n?o o c?digo deixa apenas o livro de G?nesis.
+#All the words of the bible, without the stop-words
 All_bible_words <- bible_w_testament %>% ungroup(Book) %>%
   unnest_tokens(word, Text) %>% filter(!word %in% stop_words$word)
 
-#inner join de todas as palavras da b?blia com as palavras do lexico "afinn"
+#inner join between the bible and the afinn dictionary
 Bible_afinn <- All_bible_words %>% filter(word %in% afinn$word)
 
-#colagem de coluna de valores do l?xico "afinn"
-#n?o consegui fazer tudo com o pipeline (melhorar)
-Bible_afinn <- left_join(Bible_afinn, afinn, by = "word") # same as Bible_afinn w the value column
+#Column bind (improve)
+Bible_afinn <- left_join(Bible_afinn, afinn, by = "word") # same as Bible_afinn with the value column
 
-#resumindo pontua??es totais por livro segundo o l?xico "afinn"
+#summarizing total score per book, with afin lexican
 book_by_values <- Bible_afinn %>% 
   group_by(Book) %>%
   mutate(All_values = sum(value),
          mean_score = All_values/n_words_by_books*1000) %>%
-  arrange(mean_score) %>%
-  book_by_values[,c(1,4,5,8:9)] %>% distinct()
-#criei uma m?trica na coluna mean_score para que a pontua??o leve em conta a diferen?a de n?mero de palavras entre os livros
+  arrange(mean_score)
+#Creates a column named mean_score, that takes into account the number of words in each book
 
-#criando um df resumido com as pontua??es de cada livro e a m?dia de pontua??o por cap?tulo (melhorar)
+#Creating an ordered df that summarize the total score of each book (improve)
 distinct_score_bible_books <- book_by_values[,c(1,4,5,8:9)] %>% distinct()
 
-### Gr?ficos
+### Graphics
 library(ggplot2)
 
-#plotando um gr?fico de barras com os livros por ordem crescente por pontua??o segundo o l?xico "afinn".
+#Plot a bar graphic with the afinn lexican score of each book in the bible, in ascending order
 distinct_score_bible_books %>% ungroup(Book) %>%
   mutate(Book = reorder(Book, mean_score)) %>%
   ggplot(aes(label = Book,x=Book, y = mean_score)) +
@@ -85,7 +83,7 @@ distinct_score_bible_books %>% ungroup(Book) %>%
   geom_bar(stat='identity',aes(fill = Testament),width=.7) +
   theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
   xlab("Books") + ylab('Score') +
-  labs(subtitle="A sentiment analisys of the Bible books", 
+  labs(subtitle="A sentiment analysis of the Bible books", 
        title= "AFINN SCORE", x = "Books") +
   scale_y_continuous(limits = c(-10,20), breaks = seq(-10,20,2)) +
   scale_fill_manual(values = c("firebrick", "dodgerblue4")) +
@@ -95,23 +93,24 @@ distinct_score_bible_books %>% ungroup(Book) %>%
 ##Romans
 Romans_analysis_chapter <- Bible_afinn %>% filter(Book == "Rom") %>%
   group_by(Chapter) %>% summarize(All_values = sum(value)) %>%
-  mutate(acumulado = cumsum(All_values)) %>%
   arrange(Chapter)
 
 
 Romans_analysis_chapter %>% group_by(Chapter) %>%
   ggplot(aes(label = Chapter)) +
-  geom_line(aes(x = Chapter, y = All_values)) +
+  geom_smooth(aes(x = Chapter, y = All_values), method="loess", se=F) +
   theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
-  xlab("Chapter") + ylab('\"AFINN" SCORE') +
+  theme_minimal(base_size = 10) +
+  labs(subtitle="A sentiment analysis of the Romans epistle", 
+       title= "AFINN SCORE", x = "Chapter",y ='Score') +
   scale_y_reverse()
   
-###B?NUS###
+###BONUS###
 #inner join de todas as palavras da b?blia com as palavras do lexico "afinn"
 Bible_nrc <- All_bible_words %>% filter(word %in% nrc$word) %>%
   left_join(nrc, by = "word") #b?blia com tolken e junto com o nrc
 head(Bible_nrc,10) %>% select(1:3,6,7)
-# nrc ? um l?xico problem?tico pois possui muitos sentimentos associados ? mesma palavra
-# E a an?lise precisa levar em conta contexto para ser acertiva quanto ? sem?ntica.
+#nrc is a problematic lexican, there is more than one sentiment associated to the same word
+# The analysis might see the context of the words to be more assertive
 
 
